2a4b84ce952d5ffe8bed4798bbf770ee2451bee4
kdo <otwmoo@gmail.com>
2015-03-08 22:03:14 +0900

    initial commit 1. spring web install 2. angualarjs install 3. karma and protractor install

---
+++ pop/src/main/webapp/test/unit/controllersSpec.js
@@ -0,0 +1,8 @@
+'use strict';
+
+/* jasmine specs for controllers go here */
+describe("A suite", function() {
+	it("contains spec with an expectation", function() {
+		expect(true).toBe(true);
+	});
+});

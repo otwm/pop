7cacf49b8cf1cd9191987d1203f6be2f94f17e49
kdo <otwmoo@gmail.com>
2015-03-25 02:25:47 +0900

    3423

--- src/main/webapp/app/todo/js/TodoListController.js
+++ src/main/webapp/app/todo/js/TodoListController.js
@@ -36,4 +36,33 @@ todoControllers = todoControllers.controller('TodoListController', [ '$scope', '
 				}
 				return count;
 			};
-		} ]);
+			
+			$scope.message = '';
+			$scope.ws;
+			$scope.echoMessages = [];
+
+			$scope.init = function() {
+				$scope.ws = new WebSocket('ws://localhost:8080/echo');
+				$scope.ws.onopen = function() {
+					console.log('websocket opened');
+				};
+				$scope.ws.onmessage = function(message) {
+					console.log(message);
+					console.log('receive message : ' + message.data);
+					$scope.echoMessages.unshift(message.data);
+					$timeout(function() {
+						$scope.$apply('echoMessages');
+					})
+				};
+				$scope.ws.onclose = function(event) {
+					console.log(event);
+					console.log('websocket closed');
+				};
+			};
+
+			$scope.send = function() {
+				$scope.ws.send($scope.message);
+			};
+
+			$scope.init();
+		}]);
